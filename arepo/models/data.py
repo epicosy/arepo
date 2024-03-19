269e3e989f17f80bca53456bda0f91f527ab58c1
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-03-19 10:54:16 -0700

    updated the ProfileModel to include single_commit attribute

--- arepo/models/data.py
+++ arepo/models/data.py
@@ -34,6 +34,7 @@ class ProfileModel(Base):
     has_code = Column('has_code', Boolean, nullable=False)
     has_exploit = Column('has_exploit', Boolean, nullable=False)
     has_advisory = Column('has_advisory', Boolean, nullable=False)
+    single_commit = Column('single_commit', Boolean, nullable=False)
     start_year = Column('start_year', Integer, nullable=False)
     end_year = Column('end_year', Integer, nullable=True)
     start_score = Column('start_score', Float, nullable=False)
